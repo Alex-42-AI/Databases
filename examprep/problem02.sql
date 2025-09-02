50bd6a183ee4c0b1cb46e3511d52d1789cab7b0a
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-09-02 22:29:39 +0300

    Update problem02.sql

--- examprep/problem02.sql
+++ examprep/problem02.sql
@@ -2,5 +2,5 @@ SELECT me.name, COUNT(m.title) AS num_movies, AVG(m.length) AS average
 FROM MovieExec me
          LEFT JOIN Movie m ON me.cert# = m.producerc#
 GROUP BY me.name
-HAVING num_movies = 0
+HAVING COUNT(m.title) = 0
     OR MAX(m.length) < 90;
